f1316d2ce1725f0b3226070e7163124cffd5c1f7
filippo199600 <pippotartaglia@gmail.com>
2023-09-25 16:11:32 +0200

    add ban text

--- results.php
+++ results.php
@@ -1,2 +1,28 @@
 <?php
 
+$text = $_GET["text"];
+
+
+
+$banWord = $_GET["banWord"];
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Page with ban words</title>
+</head>
+<body>
+  <h1>Questo è il testo che hai scritto e la sua lunghezza:</h1>
+  <p><?php echo $inputText = $text . strlen($text); ?></p>
+  <h1>Questo è il testo che hai scritto con i caratteri bannati:
+  </h1>
+  <p></p>
+  <p> <?php echo $banWordText = str_replace($banWord,"***",$text); echo strlen($banWordText) ?></p>
+</body>
+</html>
